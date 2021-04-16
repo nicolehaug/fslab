3b17cf7fe9b842ed4fe63aa70322d302c491ddd6
Nicole <33235578+nicolehaug@users.noreply.github.com>
2021-04-15 19:39:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "nic-haug"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 }
 
 # Use AWS Terraform provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 # Create EC2 instance
